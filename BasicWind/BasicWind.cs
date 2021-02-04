994246e8867c99ae1c5e0fd559a255fa70b0af4b
Sekker2k4 <59373642+Sekker2k4@users.noreply.github.com>
2021-02-04 13:09:08 +0100

    Add files via upload

---
+++ BasicWind/BasicWind.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine.Rendering;
+using UnityEngine;
+
+namespace HDRPSamples
+{
+    [System.Serializable]
+    public class BasicWind : VolumeComponent
+    {
+        [Header("General Parameters")]
+        public FloatParameter attenuation = new FloatParameter(1.0f);
+        [Header("Noise Parameters")]
+        [Tooltip("Texture used for wind turbulence")]
+        public TextureParameter noiseTexture = new TextureParameter(null);
+        [Tooltip("Size of one world tiling patch of the Noise Texture, for bending trees")]
+        public FloatParameter flexNoiseWorldSize = new FloatParameter(175.0f);
+        [Tooltip("Size of one world tiling patch of the Noise Texture, for leaf shivering")]
+        public FloatParameter shiverNoiseWorldSize = new FloatParameter(10.0f);
+        [Header("Gust Parameters")]
+        [Tooltip("Texture used for wind gusts")]
+        public TextureParameter gustMaskTexture = new TextureParameter(null);
+        [Tooltip("Size of one world tiling patch of the Gust Texture, for leaf shivering")]
+        public FloatParameter gustWorldSize = new FloatParameter(600.0f);
+        [Tooltip("Wind Gust Speed in Kilometers per hour")]
+        public FloatParameter gustSpeed = new FloatParameter(50.0f);
+        [Tooltip("Wind Gust Influence on trees")]
+        public FloatParameter gustScale = new FloatParameter(1.0f);
+    }
+}
---
+++ BasicWind/Editor/BasicWindDataEditor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+
+namespace HDRPSamples
+{
+    [CustomEditor(typeof(BasicWindData))]
+    public class BasicWindDataEditor : Editor
+    {   
+        public override void OnInspectorGUI()
+        {
+        }
+    }
+}   
